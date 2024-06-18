65c92ae4f360b058b406997fc0ee5ceaefd245c7
katerina-shche <asti.katerina@gmail.com>
2024-06-18 08:14:13 +0200

    transfer from desktop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import './App.css'
+import TripList from './components/TripList'
+import { useState } from 'react'
 
 function App() {
+  const [showTrips, setShowTrips] = useState(true)
+
   return (
     <div className="App">
-     <p>hi,there</p>
+      <button onClick={() => setShowTrips(false)}>hide trips</button>
+      {showTrips && <TripList /> }
     </div>
   );
 }
 
-export default App;
+export default App
